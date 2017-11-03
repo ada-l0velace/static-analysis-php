b955d4aefb11379adce73adda8b7feaf326ed46e
masihi <madrugadjn@gmail.com>
2017-11-03 18:23:45 +0000

    Update Tree structure

--- Tree.py
+++ Tree.py
@@ -2,9 +2,10 @@ class Tree:
     def __init__(self, root):
         self.root = root
 
-class Node:
+class Leaf:
 
-    def __init__(self, value, children = []):
+    def __init__(self, value, parent = None, children = []):
+        self.parent = parent
         self.value = value
         if children != []:
             self.children = [children]
@@ -14,20 +15,26 @@ class Node:
     def __repr__(self):
         return str(self.value)
 
+    def getParent(self):
+        return self.parent
+
+    def setParent(self, Leaf):
+        self.parent = Leaf
+
     def getValue(self):
         return self.value
-        
+
     def setValue(self, value):
         self.value = value
-        
+
+    def getChildren(self):
+        return self.children
+
     def setChildren(self, children):
         self.children = children
 
     def addChild(self, child):
         self.children += [child]
-
-    def getChildren(self):
-        return self.children
 
     def getChild(self, index):
         return self.children[index]
