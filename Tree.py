00c9b6a72ff1fb638c208ae414abe83b130dbdae
nucleardannyd <daniel.leitao@ist.utl.pt>
2017-11-08 00:43:48 +0000

    Added Leaf insertion to Tree

--- Tree.py
+++ Tree.py
@@ -2,18 +2,32 @@ class Tree:
     def __init__(self, root):
         self.root = root
 
+    def insertLeaf(self, leaf):
+        self.root.addChild(leaf)
+
+
 class Leaf:
 
-    def __init__(self, value, parent = None, children = []):
+    def __init__(self, value, parent=None, children=[]):
         self.parent = parent
         self.value = value
-        if children != []:
-            self.children = [children]
-        else:
-            self.children = children
+        self.children = list(children)
 
     def __repr__(self):
         return str(self.value)
 
-    def addChild(self, child):
-        self.children += [child]
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def addChild(self, leaf):
+        if self == leaf.parent:
+            self.children += [leaf]
+        elif self.children == []:
+            return
+        else:
+            for l in self.children:
+                if l == leaf.parent:
+                    l.children += [leaf]
+                    return
+                else:
+                    l.addChild(leaf)
